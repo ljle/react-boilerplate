cdccd362ff06fd56da36bb089babc731c5101603
Luis León <ljle.06@gmail.com>
2020-01-12 16:42:56 -0300

    Add comma-dangle rule to ESLint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,25 @@
 module.exports = {
   env: {
     browser: true,
-    es6: true,
+    es6: true
   },
   extends: ['plugin:react/recommended', 'airbnb'],
   globals: {
     Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    SharedArrayBuffer: 'readonly'
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true,
+      jsx: true
     },
     ecmaVersion: 2018,
-    sourceType: 'module',
+    sourceType: 'module'
   },
   plugins: ['react'],
   rules: {
     'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
     semi: [1, 'never'],
     'arrow-parens': [1, 'as-needed'],
-  },
+    'comma-dangle': [1, 'never']
+  }
 }
